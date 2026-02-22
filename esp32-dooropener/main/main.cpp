ff752ad365f2881d54c3636aa1096d1095582ce2
Hagen Hentschel <73716959+cptnpopcorn@users.noreply.github.com>
2026-02-22 10:26:57 +0100

    add rapidjson lib for door opener

--- esp32-dooropener/main/main.cpp
+++ esp32-dooropener/main/main.cpp
@@ -6,6 +6,9 @@
 #include "wifi_connection.h"
 #include "wifi_station.h"
 
+#include "rapidjson/reader.h"
+#include "rapidjson/error/en.h"
+
 #include <chrono>
 #include <exception>
 #include <iostream>
@@ -13,7 +16,33 @@
 #include <thread>
 #include <utility>
 
+class OpenMessageBuilder final : public rapidjson::BaseReaderHandler<rapidjson::UTF8<>, OpenMessageBuilder>
+{
+public:
+
+	bool Key(const char* str, rapidjson::SizeType length, bool)
+	{
+		// TODO
+		return true;
+	}
+
+	bool String(const char* str, rapidjson::SizeType length, bool)
+	{
+		// TODO
+		return true;
+	}
+
+	bool Bool(bool b)
+	{
+		// TODO
+		return true;
+	}
+};
+
 extern "C" void app_main(void)
 {
 	check(esp_event_loop_create_default());
+
+	// TODO
+	OpenMessageBuilder message {};
 }

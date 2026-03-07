80db834b8ed6baf5663ac13b8ee7a5586c9af2c8
Hagen Hentschel <73716959+cptnpopcorn@users.noreply.github.com>
2026-03-07 14:51:57 +0100

    cleanup obsolete code

--- esp32-cardreader/main/main.cpp
+++ esp32-cardreader/main/main.cpp
@@ -31,18 +31,6 @@
 using namespace std;
 using namespace std::chrono;
 
-void check(esp_err_t err) {
-  if (err == ESP_OK) return;
-  throw system_error((int)err, system_category());
-}
-
-void check(esp_err_t err, const string &what) {
-  if (err == ESP_OK) return;
-  throw system_error((int)err, system_category(), what);
-}
-
-void error(const std::string &what) { throw runtime_error(what); }
-
 #ifdef CONFIG_MQTT_USE_TLS
 span<const uint8_t> get_ca_crt()
 {
